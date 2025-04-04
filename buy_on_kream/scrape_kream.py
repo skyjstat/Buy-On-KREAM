5131bc55a89ada27a0dd5a5bfa059b7fb98c5944
Haneul Jeong <158139469+skyjstat@users.noreply.github.com>
2025-04-04 19:00:03 +0900

    Update scrape_kream.py

--- buy_on_kream/scrape_kream.py
+++ buy_on_kream/scrape_kream.py
@@ -1,6 +1,5 @@
 from utils_module.utils import is_error, price_real, dict_cat, dict_gender, shoes_size
 import pandas as pd
-import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver               
 from selenium.webdriver.common.by import By  
