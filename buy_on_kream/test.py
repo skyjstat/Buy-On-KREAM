e22ea9b2f6a65dd22812889df2f4c8e90bc6f2c7
Haneul Jeong <158139469+skyjstat@users.noreply.github.com>
2025-04-04 17:01:03 +0900

    Update test.py

--- buy_on_kream/test.py
+++ buy_on_kream/test.py
@@ -2,9 +2,12 @@ import sys
 import os
 import pandas as pd
 
-df = pd.DataFrame({
-    'a': [1, 2, 3],
-    'b': ['x', 'y', 'z']
-})
-df.to_csv("buy_on_kream/data/test_df.csv")
+def main():
+    df = pd.DataFrame({
+        'a': [1, 2, 3],
+        'b': ['x', 'y', 'z']
+    })
+    return df
 
+if __name__ == "__main__":
+    main()
