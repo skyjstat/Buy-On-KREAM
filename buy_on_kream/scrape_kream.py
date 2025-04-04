7ef30dedb9e7943830248398fda4ea1308c9baed
Haneul Jeong <158139469+skyjstat@users.noreply.github.com>
2025-04-04 19:25:41 +0900

    Update scrape_kream.py

--- buy_on_kream/scrape_kream.py
+++ buy_on_kream/scrape_kream.py
@@ -158,8 +158,10 @@ def post_processing(item_df, kream_df):
 def main():
     options = Options()
     options.add_argument('--headless=new')
-    # options.add_argument('--no-sandbox')
-    # options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36')
+
 
     driver = webdriver.Chrome(options=options); print("### 웹드라이버 생성 완료 ###")
     # driver = webdriver.Chrome()
