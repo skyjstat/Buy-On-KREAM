4d8bdbba97c219c946f1fc2867a512cdafc7eed0
Haneul Jeong <158139469+skyjstat@users.noreply.github.com>
2025-04-04 17:12:46 +0900

    Update test.py

--- buy_on_kream/test.py
+++ buy_on_kream/test.py
@@ -4,6 +4,9 @@ import pandas as pd
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 SAVE_PATH = os.path.join(BASE_DIR, "../data/test_df.csv")
 
+print(BASE_DIR)
+print(SAVE_PATH)
+
 df = pd.DataFrame({
     'a': [1, 2, 3],
     'b': ['x', 'y', 'z']
