37b3aa807d2ddaa391cae40c7285a6f47ebc3881
Haneul Jeong <158139469+skyjstat@users.noreply.github.com>
2025-04-04 16:08:47 +0900

    Update test.py

--- buy_on_kream/test.py
+++ buy_on_kream/test.py
@@ -2,8 +2,4 @@ import sys
 import os
 import pandas as pd
 
-def get_path(relative_path):
-    BASE_DIR = os.path.abspath(os.path.dirname(__file__)) 
-    return os.path.normpath(os.path.join(BASE_DIR, relative_path)) 
-
-pd.DataFrame().to_csv(get_path("../data/test_df.csv"))
+pd.DataFrame().to_csv("../data/test_df.csv")
