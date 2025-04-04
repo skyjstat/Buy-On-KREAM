fe22cecec7b712975c117597b0169ef5763c6d1d
Haneul Jeong <158139469+skyjstat@users.noreply.github.com>
2025-04-04 14:55:02 +0900

    Update scrape_kream.py

--- buy_on_kream/scrape_kream.py
+++ buy_on_kream/scrape_kream.py
@@ -1,3 +1,15 @@
+from utils_module.utils import is_error, price_real, dict_cat, dict_gender, shoes_size
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver               
+from selenium.webdriver.common.by import By  
+from datetime import datetime 
+import time
+import re
+import warnings
+warnings.filterwarnings('ignore')
+from utils_module.utils import make_query, format_price, shoes_size
 import os
 
 kream_id = os.environ.get("KREAM_ID")
